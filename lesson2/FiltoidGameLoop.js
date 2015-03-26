a731b9ae6b8e543914baa771d638d7d698498b39
Phil Jeffes <philjeffes@hotmail.com>
2015-03-26 19:02:18 +0000

    Added in lesson 2

--- lesson2/FiltoidGameLoop.js
+++ lesson2/FiltoidGameLoop.js
@@ -3,13 +3,13 @@
 This is a sample game loop to provide a reasonable update control mechanism for
 animations and game logic. Please feel free to do whatever the hell you like with this code. It is free as beer
 and free as in speech. If you use this code you do so at your own risk and I accept no responsibility
-whatsoever for it.
+whatsoever for it. 
 
+Thank and have fun at LD25 :)
 */
 //Globals
-var FPS = 25; // target frames per second
+var FPS = 30; // target frames per second
 var SECONDSBETWEENFRAMES = 1 / FPS;
-
 var ctx = null; // Useful to have a global reference for measuring fonts for instance
 var canvas = null; // The main drawing area
 var currentTime = 0; // For debugging - you can store the current time and see how it's changed
@@ -19,27 +19,31 @@ var mySquare = null;
 function loadGame(){
 	canvas = document.getElementById('canvas');
 	ctx = canvas.getContext('2d');
-
+	
+	// Let's register for the key events
+	$(document).keydown(onKeyDown);
+	$(document).keyup(onKeyUp);
+	
 	// Do setup code here - make resources/assign things etc
 	mySquare = new Square();
-
+	
 	// The following line sets up the game loop
-	setInterval(update, SECONDSBETWEENFRAMES * 500);
+	setInterval(update, SECONDSBETWEENFRAMES * 500);	
 }
 
 function update(){
 	// Store the time - for debugging purposes mostly
 	currentTime += SECONDSBETWEENFRAMES;
-
+	
 	// Clear the drawing area
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	ctx.save();
-
+	
 	//Do your game updates here
 	mySquare.update();
 	mySquare.draw(ctx);
-
+	
 	//Do you drawing here - make your resources draw themselves
-
+	
 	ctx.restore();
 }
--- lesson2/Square.js
+++ lesson2/Square.js
@@ -3,12 +3,38 @@ function Square(){
 	this.update = sqUpdate;
 	this.draw = sqDraw;
 	this.rot = 0;
-
+	
 	this.SIZE = 40;
-
+	
 }
 
 function sqUpdate(){
+	
+	// Check the keys that are down
+	if(isKeyPressed('A')){
+		this.loc.x-=1;
+	}
+	if(isKeyPressed('D')){
+		this.loc.x+=1;
+	}
+	if(isKeyPressed('W')){
+		this.loc.y-=1;
+	}
+	if(isKeyPressed('S')){
+		this.loc.y+=1;
+	}
+	
+	// Boundary checking - don't go outside the bounds of the canvas
+	if(this.loc.x>600)
+		this.loc.x=600;
+	if(this.loc.x<0)
+		this.loc.x=0;
+	if(this.loc.y>400)
+		this.loc.y=400;
+	if(this.loc.y<0)
+		this.loc.y=0;
+	
+	// Do the rotation
 	this.rot += 0.05;
 	if(this.rot>2*Math.PI)
 		this.rot-=2*Math.PI;
@@ -17,13 +43,13 @@ function sqUpdate(){
 }
 
 function sqDraw(ctx){
-
+	
 	// Figure out the points of rotation multiplied by size
 	var pt1 = new Location(this.SIZE*Math.sin(this.rot+(Math.PI/4)),this.SIZE*Math.cos(this.rot+(Math.PI/4)));
 	var pt2 = new Location(this.SIZE*Math.sin(this.rot+(Math.PI*3/4)),this.SIZE*Math.cos(this.rot+(Math.PI*3/4)));
 	var pt3 = new Location(this.SIZE*Math.sin(this.rot+(Math.PI*5/4)),this.SIZE*Math.cos(this.rot+(Math.PI*5/4)));
 	var pt4 = new Location(this.SIZE*Math.sin(this.rot+(Math.PI*7/4)),this.SIZE*Math.cos(this.rot+(Math.PI*7/4)));
-
+	
 	// Apply the translation to our actual location
 	pt1.x += this.loc.x;
 	pt1.y += this.loc.y;
@@ -33,16 +59,16 @@ function sqDraw(ctx){
 	pt3.y += this.loc.y;
 	pt4.x += this.loc.x;
 	pt4.y += this.loc.y;
-
+	
 	// Draw a trianle rotated around a point
-	ctx.strokeStyle = "#0000FF"; // Set line color to black
+	ctx.strokeStyle = "#000000"; // Set line color to black
 	ctx.beginPath(); //Start the drawing
-
+	
 	ctx.moveTo(pt1.x,pt1.y);
 	ctx.lineTo(pt2.x,pt2.y);
 	ctx.lineTo(pt3.x,pt3.y);
 	ctx.lineTo(pt4.x,pt4.y);
 	ctx.lineTo(pt1.x,pt1.y);
-
+	
 	ctx.stroke(); // End the drawing
 }
